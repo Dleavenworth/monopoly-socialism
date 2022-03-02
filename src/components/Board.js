ba3533533f8efaaa3e5af2267459612fddf55764
David Leavenworth <dleavenworth3@gmail.com>
2022-03-02 12:00:13 -0800

    Dynamic board generation works

--- src/components/Board.js
+++ src/components/Board.js
@@ -1,37 +1,43 @@
-import React from 'react'
+import React, { useRef, useEffect, useMemo } from 'react'
 import Cell from './Cell'
 import Grid from '@mui/material/Grid'
 import Box from '@mui/material/Box'
 
-const gridSize = 10
-const oneCell = <Cell/>
-const duplicate = (x, n) => Array.from(new Array(n), () => x)
-const nCells = duplicate(oneCell, gridSize)
+const gridSize = 5
 
 export default function Board() {
 
+    let squareRefs = []
+
+    const makeBoard = () => {
+        let col = 1
+        let row = 1
+        let squares = []
+
+        const totalSquares = (gridSize*2+((gridSize-2)*2))
+
+        for(let i = 0; i < totalSquares; i++) {
+            if(i < gridSize) {
+                col++
+            }
+            else if(i < 2 * gridSize-1) {
+                row++
+            }
+            else if(i < 3 * gridSize-2) {
+                col--
+            }
+            else {
+                row--
+            }
+            let tag = <Cell key={i} column={col} row={row}/>
+            squares.push(tag)
+        }
+        return squares
+    }
+
     return (
         <Box sx={{display: "grid", gap: 0, gridTemplateColumns: 'repeat(5, 0fr)', gridTemplateRows: 'repeat(5, 0fr'}}>
-            <Cell column="1" row="1"/>
-            <Cell column="1" row="2"/>
-            <Cell column="1" row="3"/>
-            <Cell column="1" row="4"/>
-            <Cell column="1" row="5"/>
-            <Cell column="2" row="1"/>
-            <Cell column="3" row="1"/>
-            <Cell column="4" row="1"/>
-            <Cell column="5" row="1"/>
-            <Cell column="5" row="2"/>
-            <Cell column="5" row="3"/>
-            <Cell column="5" row="4"/>
-            <Cell column="5" row="5"/>
-            <Cell column="2" row="5"/>
-            <Cell column="3" row="5"/>
-            <Cell column="4" row="5"/> 
+            {makeBoard()}
         </Box>
     )
 }
-            /*<Box sx={{border: '1px solid black'}} className="cell" gridColumn="7" gridRow="8"/>
-            <Box sx={{border: '1px solid black'}} className="cell" gridColumn="8" gridRow="8"/>
-            <Box sx={{border: '1px solid black'}} className="cell" gridColumn="9" gridRow="8"/>
-            <Box sx={{border: '1px solid black'}} className="cell" gridColumn="10" gridRow="9"/>*/
