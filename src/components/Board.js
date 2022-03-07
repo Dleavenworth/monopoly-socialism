39f91aa5102084f1aaa37f790a97e8d6822c86a0
David Leavenworth <dleavenworth3@gmail.com>
2022-03-07 11:37:19 -0800

    Finally have the array of refs working

--- src/components/Board.js
+++ src/components/Board.js
@@ -1,52 +1,78 @@
-import React, { useRef, useEffect, useMemo } from 'react'
-import Cell from './Cell'
-import Grid from '@mui/material/Grid'
-import Box from '@mui/material/Box'
+import React, {useEffect, useRef} from "react"
+import Cell from "./Cell"
+import { Button, Box } from "@mui/material"
 
-const gridSize = 9
-let templateString = 'repeat(' + gridSize + ', 0fr)'
 
-export default function Board() {
-    let squares = []
+const Board = () => {
+    let playerLoc = 1
+    const gridSize = 9
+    let templateString = "repeat(" + gridSize + ", 0fr)"
+	let squares = []
+    let squareRefs = useRef([])
 
-    const makeBoard = () => {
-        let col = 1
-        let row = 2
+	const makeBoard = () => {
+		let col = 1
+		let row = 2
 
-        const totalSquares = (gridSize*2+((gridSize-2)*2))
+		const totalSquares = gridSize * 2 + (gridSize - 2) * 2
 
-        for(let i = 0; i < totalSquares; i++) {
-            if(i < gridSize) {
-                col++
-            }
-            else if(i < 2 * gridSize-1) {
-                row++
-            }
-            else if(i < 3 * gridSize-2) {
-                col--
-            }
-            else {
-                row--
-            }
+		for (let i = 0; i < totalSquares; i++) {
+			if (i < gridSize) {
+				col++
+			} else if (i < 2 * gridSize - 1) {
+				row++
+			} else if (i < 3 * gridSize - 2) {
+				col--
+			} else {
+				row--
+			}
 
-            const square = {
-                col: col,
-                row: row,
-                player: false
-            }
-            
-            squares.push(square)
-        }
+			const square = {
+				col: col,
+				row: row,
+				player: i === 1 ? true : false,
+			}
 
-        console.log(templateString)
-        return squares.map((curSquare, i) => {
-            return <Cell key={i} row={curSquare.row} column={curSquare.col}/>
-        })
+			squares.push(square)
+		}
+        return displaySquares()
+	}
+		const displaySquares = () => {
+            console.log(squares)
+            return squares.map((curSquare, i) => {
+			return (
+				    <Cell
+                        key={i}
+					    player={curSquare.player}
+                        ref={el => (squareRefs.current = [...squareRefs.current, el])}
+					    row={curSquare.row}
+					    column={curSquare.col}
+				    />
+			)
+		})
     }
 
-    return (
-        <Box sx={{display: "grid", gap: 0, gridTemplateColumns: templateString, gridTemplateRows: templateString}}>
-            {makeBoard()}
-        </Box>
-    )
+        const movePlayer = () => {
+            squares[playerLoc] = false
+            playerLoc++
+            squares[playerLoc] = true
+            console.log(squareRefs)
+        }
+
+	return (
+		<Box>
+			<Button onClick={movePlayer}>Press me to move</Button>
+			<Box
+				sx={{
+					display: "grid",
+					gap: 0,
+					gridTemplateColumns: templateString,
+					gridTemplateRows: templateString,
+				}}
+			>
+				{makeBoard()}
+			</Box>
+		</Box>
+	)
 }
+export default Board
--- src/components/Cell.js
+++ src/components/Cell.js
@@ -1,9 +1,11 @@
-import React from 'react';
+import React, {forwardRef} from 'react';
 import Box from '@mui/material/Box'
 
-export default function Cell(props) {
+const Cell = forwardRef((props, ref) => {
     console.log(props)
     return (
-        <Box sx={{border: "1px solid black", width: "100px", height: "100px"}}  gridRow={props.row} gridColumn={props.column}/>
+        <Box ref={ref} sx={{border: "1px solid black", width: "100px", height: "100px", backgroundColor: props.player ? "black" : "white"}}  
+        gridRow={props.row} gridColumn={props.column}/>
     )
-} 
+})
+export default Cell
