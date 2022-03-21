1adc22ae48ff783e8ba6dbf2750323e34b86b0f2
David Leavenworth <dleavenworth3@gmail.com>
2022-03-21 12:06:04 -0700

    Small formatting changes

--- src/components/Board.js
+++ src/components/Board.js
@@ -1,100 +1,100 @@
-import React, { useEffect, useRef, useState, useMemo, useReducer} from "react";
+import React, { useEffect, useRef, useState, useMemo, useReducer } from "react";
 import Cell from "./Cell";
 import { Button, Box } from "@mui/material";
 
 const Board = () => {
-    const gridSize = 9;
-    const [players, setPlayers] = useState([{num: 1, location: 1}, {num: 2, location: 1}, {num: 3, location: 1}, {num: 4, location: 1}]);
-    let templateString = "repeat(" + gridSize + ", 0fr)";
-    let squareRefs = useRef([]);
+	const gridSize = 9;
+	const [players, setPlayers] = useState([
+		{ num: 1, location: 1 },
+		{ num: 2, location: 1 },
+		{ num: 3, location: 1 },
+		{ num: 4, location: 1 },
+	]);
+	let templateString = "repeat(" + gridSize + ", 0fr)";
+	let squareRefs = useRef([]);
 	const curPlayerTurn = useRef(1);
 
+	const makeBoard = () => {
+		let col = 1;
+		let row = 2;
 
-    const makeBoard = () => {
-        let col = 1;
-        let row = 2;
+		const totalSquares = gridSize * 2 + (gridSize - 2) * 2;
+		let newSquares = [];
 
-        const totalSquares = gridSize * 2 + (gridSize - 2) * 2;
-        let newSquares = [];
+		for (let i = 0; i < totalSquares; i++) {
+			if (i < gridSize) {
+				col++;
+			} else if (i < 2 * gridSize - 1) {
+				row++;
+			} else if (i < 3 * gridSize - 2) {
+				col--;
+			} else {
+				row--;
+			}
 
-        for (let i = 0; i < totalSquares; i++) {
-            if (i < gridSize) {
-                col++;
-            } else if (i < 2 * gridSize - 1) {
-                row++;
-            } else if (i < 3 * gridSize - 2) {
-                col--;
-            } else {
-                row--;
-            }
+			const square = {
+				col: col,
+				row: row,
+			};
 
-            const square = {
-                col: col,
-                row: row,
-                //player: i === 1 ? true : false,
-            };
+			newSquares.push(square);
+		}
+		return newSquares;
+	};
 
-            newSquares.push(square);
-        }
-        return newSquares;
-    };
+	const [squares, setSquares] = useState(makeBoard());
 
-    const [squares, setSquares] = useState(makeBoard());
-
-    // Make an array of booleans where the index corresponds to each cell, then set the player field to this boolean and change the boolean whenever the player moves
-    const displaySquares = () => {
-        //console.log(squares);
-        return squares.map((curSquare, i) => {
-            return (
-                <Cell
-                    key={i}
+	// Make an array of booleans where the index corresponds to each cell, then set the player field to this boolean and change the boolean whenever the player moves
+	const displaySquares = () => {
+		//console.log(squares);
+		return squares.map((curSquare, i) => {
+			return (
+				<Cell
+					key={i}
 					squareNum={i}
-                    players={players}
-                    ref={(el) =>
-                        (squareRefs.current = [...squareRefs.current, el])
-                    }
-                    row={curSquare.row}
-                    column={curSquare.col}
-                />
-            );
-        });
-    };
+					players={players}
+					ref={(el) => (squareRefs.current = [...squareRefs.current, el])}
+					row={curSquare.row}
+					column={curSquare.col}
+				/>
+			);
+		});
+	};
 
-    useEffect(() => {
-        console.log(squares);
-    }, [squares]);
+	useEffect(() => {
+		console.log(squares);
+	}, [squares]);
 
-    const movePlayer = () => {
-        console.log("Player moving is: " + curPlayerTurn.current)
-        console.log(players[curPlayerTurn.current-1])
-		let newPlayers = players
+	const movePlayer = () => {
+		console.log("Player moving is: " + curPlayerTurn.current);
+		console.log(players[curPlayerTurn.current - 1]);
+		let newPlayers = players;
 
-		newPlayers[curPlayerTurn.current-1].location++;
+		newPlayers[curPlayerTurn.current - 1].location++;
 
-        setPlayers([...newPlayers]);
+		setPlayers([...newPlayers]);
 
-        if(curPlayerTurn.current >= 4) {
-		    curPlayerTurn.current = 1;
-        }
-        else {
-            curPlayerTurn.current++;
-        }
-    };
+		if (curPlayerTurn.current >= 4) {
+			curPlayerTurn.current = 1;
+		} else {
+			curPlayerTurn.current++;
+		}
+	};
 
-    return (
-        <Box>
-            <Button onClick={movePlayer}>Press me to move</Button>
-            <Box
-                sx={{
-                    display: "grid",
-                    gap: 0,
-                    gridTemplateColumns: templateString,
-                    gridTemplateRows: templateString,
-                }}
-            >
-                {displaySquares()}
-            </Box>
-        </Box>
-    );
+	return (
+		<Box>
+			<Button onClick={movePlayer}>Press me to move</Button>
+			<Box
+				sx={{
+					display: "grid",
+					gap: 0,
+					gridTemplateColumns: templateString,
+					gridTemplateRows: templateString,
+				}}
+			>
+				{displaySquares()}
+			</Box>
+		</Box>
+	);
 };
 export default Board;
--- src/components/Cell.js
+++ src/components/Cell.js
@@ -1,37 +1,39 @@
 import React, { useEffect, forwardRef, useState } from "react";
-import {Box, List, ListItem} from "@mui/material"
+import { Box, List, ListItem } from "@mui/material";
 
 const Cell = forwardRef((props, ref) => {
+	useEffect(() => {
+		console.log("player changed");
+	}, [props]);
 
-    useEffect(() => {
-        console.log("player changed")
-    }, [props])
+	console.log(props.players[1] === props.squareNum);
 
-    console.log(props.players[1] === props.squareNum)
-
-    return (
-        <Box
-            ref={ref}
-            sx={{
-                border: "1px solid black",
-                width: "100px",
-                height: "100px",
-                //backgroundColor: props.player ? "black" : "white",
-            }}
-            gridRow={props.row}
-            gridColumn={props.column}
-        >
-            <List>
-            {props.players.map((curPlayer, i) => {
-                if(props.players[i].location === props.squareNum) {
-                return <ListItem key={i}>Player {props.players[i].num} is here </ListItem>
-                }
-                else {
-                    return null
-                }
-            })}
-            </List>
-            </Box>
-    );
+	return (
+		<Box
+			ref={ref}
+			sx={{
+				border: "1px solid black",
+				width: "100px",
+				height: "100px",
+				//backgroundColor: props.player ? "black" : "white",
+			}}
+			gridRow={props.row}
+			gridColumn={props.column}
+		>
+			<List>
+				{props.players.map((curPlayer, i) => {
+					if (props.players[i].location === props.squareNum) {
+						return (
+							<ListItem key={i}>
+								Player {props.players[i].num} is here{" "}
+							</ListItem>
+						);
+					} else {
+						return null;
+					}
+				})}
+			</List>
+		</Box>
+	);
 });
 export default Cell;
