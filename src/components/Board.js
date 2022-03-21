f97ccbd4d9d734a7b9a706b22671b21c579c1901
David Leavenworth <dleavenworth3@gmail.com>
2022-03-21 01:38:38 -0700

    You can now move around the board

--- src/components/Board.js
+++ src/components/Board.js
@@ -1,96 +1,124 @@
-import React, {useEffect, useRef, useState, useMemo} from "react"
-import Cell from "./Cell"
-import { Button, Box } from "@mui/material"
-
+import React, { useEffect, useRef, useState, useMemo, useReducer} from "react";
+import Cell from "./Cell";
+import { Button, Box } from "@mui/material";
 
 const Board = () => {
-    let playerLoc = 1
-    const gridSize = 9
-    let templateString = "repeat(" + gridSize + ", 0fr)"
-	const [squares, setSquares] = useState([])
-    let squareRefs = useRef([])
+    const gridSize = 9;
+    const [playerLoc, setPlayerLoc] = useState(1);
+    let templateString = "repeat(" + gridSize + ", 0fr)";
+    let squareRefs = useRef([]);
 
-	useEffect(() => {
-		makeBoard()
-	}, [])
 
-	const makeBoard = () => {
-		let col = 1
-		let row = 2
+    const makeBoard = () => {
+        let col = 1;
+        let row = 2;
 
-		const totalSquares = gridSize * 2 + (gridSize - 2) * 2
+        const totalSquares = gridSize * 2 + (gridSize - 2) * 2;
+        let newSquares = [];
 
-		for (let i = 0; i < totalSquares; i++) {
-			if (i < gridSize) {
-				col++
-			} else if (i < 2 * gridSize - 1) {
-				row++
-			} else if (i < 3 * gridSize - 2) {
-				col--
-			} else {
-				row--
-			}
+        for (let i = 0; i < totalSquares; i++) {
+            if (i < gridSize) {
+                col++;
+            } else if (i < 2 * gridSize - 1) {
+                row++;
+            } else if (i < 3 * gridSize - 2) {
+                col--;
+            } else {
+                row--;
+            }
 
-			const square = {
-				col: col,
-				row: row,
-				player: i === 1 ? true : false,
-			}
+            const square = {
+                col: col,
+                row: row,
+                player: i === 1 ? true : false,
+            };
 
-			let newSquares = squares
-			newSquares.push(square)
-
-			console.log("updated new squares")
-
-			setSquares(newSquares)
-		}
-        //return displaySquares()
-	}
-
-	// Make an array of booleans where the index corresponds to each cell, then set the player field to this boolean and change the boolean whenever the player moves
-		const displaySquares = () => {
-            console.log(squares)
-            return squares.map((curSquare, i) => {
-			return (
-				    <Cell
-                        key={i}
-					    player={squares[i].player}
-                        ref={el => (squareRefs.current = [...squareRefs.current, el])}
-					    row={curSquare.row}
-					    column={curSquare.col}
-				    />
-			)
-		})
-    }
-
-        const movePlayer = () => {
-
-			let newSquare = squares[playerLoc]
-			newSquare.player = false
-			setSquares(newSquare)
-
-            playerLoc++
-
-			newSquare = squares[playerLoc]
-			newSquare.player = true
-			setSquares(newSquare)
-
-            console.log(squareRefs.current[playerLoc])
-			console.log(squares)
+            newSquares.push(square);
         }
+        return newSquares;
+    };
 
-	return (
-		<Box>
-			<Button onClick={movePlayer}>Press me to move</Button>
-			<Box
-				sx={{
-					display: "grid",
-					gap: 0,
-					gridTemplateColumns: templateString,
-					gridTemplateRows: templateString,
-				}}
-			>
-            {squares.map((curSquare, i) => {
+    const [squares, setSquares] = useState(makeBoard());
+
+    useEffect(() =>{
+        // Run this code after playerLoc is updated in movePlayer, that is, once playerLoc is incremented by 1 run this code
+        // There might be a better way to do this but I couldn't find one
+        let newSquares = squares
+        let newSquare = squares[playerLoc];
+        newSquare.player = true;
+        newSquares[playerLoc] = newSquare;
+        console.log(newSquares);
+        setSquares([...newSquares]);
+
+        console.log("MOVING CODE");
+        console.log(squareRefs.current[playerLoc]);
+        console.log("Playerloc is: " + playerLoc);
+    }, [playerLoc])
+
+    // Make an array of booleans where the index corresponds to each cell, then set the player field to this boolean and change the boolean whenever the player moves
+    const displaySquares = () => {
+        //console.log(squares);
+        return squares.map((curSquare, i) => {
+            return (
+                <Cell
+                    key={i}
+                    player={squares[i].player}
+                    ref={(el) =>
+                        (squareRefs.current = [...squareRefs.current, el])
+                    }
+                    row={curSquare.row}
+                    column={curSquare.col}
+                />
+            );
+        });
+    };
+
+    useEffect(() => {
+        console.log(squares);
+    }, [squares]);
+
+    const movePlayer = () => {
+        console.log("Playerloc is: " + playerLoc);
+        let newSquare = squares[playerLoc];
+        newSquare.player = false;
+        let newSquares = squares;
+        newSquares[playerLoc] = newSquare;
+        //setSquares(newSquares)
+
+        setPlayerLoc(playerLoc + 1);
+
+        /*newSquare = squares[playerLoc];
+        newSquare.player = true;
+        newSquares[playerLoc] = newSquare;
+        console.log(newSquares);
+        setSquares([...newSquares]);
+
+        console.log("MOVING CODE");
+        console.log(squareRefs.current[playerLoc]);
+        console.log("Playerloc is: " + playerLoc);*/
+        //console.log(squares)
+    };
+
+    return (
+        <Box>
+            <Button onClick={movePlayer}>Press me to move</Button>
+            <Box
+                sx={{
+                    display: "grid",
+                    gap: 0,
+                    gridTemplateColumns: templateString,
+                    gridTemplateRows: templateString,
+                }}
+            >
+                {displaySquares()}
+            </Box>
+        </Box>
+    );
+};
+export default Board;
+
+/*----------------------------------------------------------------
+{squares ? squares.map((curSquare, i) => {
 			return (
 				    <Cell
                         key={i}
@@ -100,9 +128,5 @@ const Board = () => {
 					    column={curSquare.col}
 				    />
 			)
-		})}
-			</Box>
-		</Box>
-	)
-}
-export default Board
+		}) : console.log(typeof squares)}
+        */
--- src/components/Cell.js
+++ src/components/Cell.js
@@ -1,12 +1,24 @@
-import React, {useEffect, forwardRef, useState} from 'react';
-import Box from '@mui/material/Box'
+import React, { useEffect, forwardRef, useState } from "react";
+import Box from "@mui/material/Box";
 
 const Cell = forwardRef((props, ref) => {
 
-    console.log(props)
+    useEffect(() => {
+        console.log("player changed")
+    }, [props])
+
     return (
-        <Box ref={ref} sx={{border: "1px solid black", width: "100px", height: "100px", backgroundColor: props.player ? "black" : "white"}}  
-        gridRow={props.row} gridColumn={props.column}/>
-    )
-})
-export default Cell
+        <Box
+            ref={ref}
+            sx={{
+                border: "1px solid black",
+                width: "100px",
+                height: "100px",
+                backgroundColor: props.player ? "black" : "white",
+            }}
+            gridRow={props.row}
+            gridColumn={props.column}
+        />
+    );
+});
+export default Cell;
